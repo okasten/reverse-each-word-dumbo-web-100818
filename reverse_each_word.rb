f576369100c0efae35dc5d0bcdfe735729e294dc
Olivia Kasten <odkasten@gmail.com>
2018-09-17 20:38:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   words = string.split(" ")
-  
-  words.collect do |word|
-    word.reverse 
-  end 
-  words.join(" ")
+  reversed = []
+  reversed = words.collect do |word|
+      word.reverse 
+    end 
+  reversed.join(" ")
 end 
